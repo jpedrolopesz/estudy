f7d6b06759ffbba94e0ed473f032c55aa5a9fd0c
jpedrolopesz <jplopeszamonelo@hotmail.com>
2023-07-06 13:31:26 -0300

    Commit

--- database/migrations/2018_12_05_174208_create_plan_features_table.php
+++ database/migrations/2018_12_05_174208_create_plan_features_table.php
@@ -16,19 +16,18 @@ class CreatePlanFeaturesTable extends Migration
     {
         Schema::create('plan_features', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('plan_id')->constrained('plans')->cascadeOnDelete();
+            $table->unsignedBigInteger('plan_id');
             $table->unsignedInteger('max_users')->nullable();
             $table->timestamps();
         });
+
+        Schema::table('plan_features', function (Blueprint $table) {
+            $table->dropForeign('plan_features_plan_id_foreign');
+        });
+
+        Schema::table('plan_features', function (Blueprint $table) {
+            $table->foreign('plan_id')->references('id')->on('plans')->cascadeOnDelete();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('plan_features');
-    }
 }
