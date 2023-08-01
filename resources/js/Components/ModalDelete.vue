4198dd6913f2b822bd74e200d61837b8f2780f35
jpedrolopesz <jplopeszamonelo@hotmail.com>
2023-08-01 11:57:49 -0300

    Commit

--- resources/js/Components/ModalDelete.vue
+++ resources/js/Components/ModalDelete.vue
@@ -12,7 +12,7 @@
           <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </TransitionChild>
 
-        <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="fixed inset-0 mb-32 z-10 overflow-y-auto">
           <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
               <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
@@ -34,7 +34,7 @@
                     </div>
                   </div>
                 </div>
-                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div class="flex items-center justify-between bg-gray-50 px-4 py-3 sm:px-6">
 
                   <slot/>
                   <button type="button" class="btn bg-white text-sm border border-gray-300 shadow-md text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" @click="open = false" ref="cancelButtonRef">
