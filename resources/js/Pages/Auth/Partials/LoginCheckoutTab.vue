0328aabd247ea7a27136e619e6642f43a879c5aa
jpedrolopesz <jplopeszamonelo@hotmail.com>
2023-08-03 19:10:57 -0300

    Commit

--- resources/js/Pages/Auth/Partials/LoginCheckoutTab.vue
+++ resources/js/Pages/Auth/Partials/LoginCheckoutTab.vue
@@ -45,6 +45,17 @@
 
     </div>
   </form>
+  <div >
+    <h3 class="font-semibold mt-4">User</h3>
+    <div class="hover:border-gray-500 hover:border-solid hover:bg-white hover:text-gray-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-900 font-medium py-3">
+      <div class="flex">
+        <span>E-mail: </span><span class="underline ml-2">user@demo.com</span>
+      </div>
+      <div class="flex">
+        <span>Password: </span><span class="underline ml-2">password</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
